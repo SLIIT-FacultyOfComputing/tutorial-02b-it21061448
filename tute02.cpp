89fcc0843d1084d3fc01591f7ffecb90a52feac1
it21061448 <87574604+it21061448@users.noreply.github.com>
2021-08-22 12:51:57 +0000

    TUTORIAL2,EX 2

--- tute02.cpp
+++ tute02.cpp
@@ -21,14 +21,24 @@ cout<<"enter employee type:";
 cin>>etype;
 cout<<"enter salary:";
 cin>>salary;
-cout<<"enter OtHrs:":
-cin<<otHrs;
+cout<<"enter OtHrs:";
+cin>>otHrs;
 
-switch(etype)
+switch(etype){
 case 1:
   otRate=1000;
   break;
 case 2:
+  otRate=1500;
+  break;
+default:
+  otRate=1700;
+  break;
+}
+netSalary=salary+otHrs*otRate;
+cout<<"net salary is:"<<netSalary<<endl;
+
+
   
    return 0;
 }
